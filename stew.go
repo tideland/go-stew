499a6a452f5b3952b6aeb6a8284f9c24b9b8e4c3
Frank Mueller <frank.mueller@themue.dev>
2023-07-23 14:11:32 +0200

    Add semver package

---
+++ stew.go
@@ -0,0 +1,26 @@
+// Tideland Go Stew
+//
+// Copyright (C) 2023 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+package stew // import "tideland.dev/go/stew"
+
+//--------------------
+// IMPORTS
+//--------------------
+
+import (
+	"tideland.dev/go/stew/semver"
+)
+
+//--------------------
+// VERSION
+//--------------------
+
+func Version() *semver.Version {
+	return semver.NewVersion(0, 1, 0, "alpha", "2023-07-23")
+}
+
+// EOF
